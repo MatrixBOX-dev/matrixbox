079d1837ae82af3768cd803f8d84c611df09a16c
matrixbox <info@t-skylt.se>
2026-09-12 21:01:39 +0200

    static navbar route in route handling module
    Added a new route for the navbar endpoint.

--- lib/ampule.py
+++ lib/ampule.py
@@ -190,3 +190,11 @@ def listen(socket):
 
 def route(rule, method='GET'):
     return lambda func: __on_request(method, rule, func)
+
+
+def _navbar(request):
+    return (200, {}, "placeholder")
+
+system_routes.append(
+    (re.compile(r"^/navbar/?$"), {"method": "GET", "func": _navbar})
+)
